3bce98b1f90834239e14b2a0be910b40d6708ec8
xendv <dvoynina.ksusha@mail.ru>
2021-04-14 00:28:14 +0300

    added 8.5.b

--- OOA-task8.5.b/TimerAnimationForm.h
+++ OOA-task8.5.b/TimerAnimationForm.h
@@ -18,10 +18,78 @@ namespace OOAtask85b {
 		TimerAnimationForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: добавьте код конструктора
-			//
+			try
+			{
+				sky = gcnew System::Drawing::Bitmap
+				(Application::StartupPath + "\\sky.bmp");
+				plane = gcnew System::Drawing::Bitmap
+				(Application::StartupPath + "\\plane.bmp");
+			}
+			catch (System::Exception^ e)
+			{
+				MessageBox::Show(
+					"Ошибка загрузки битового образа: "
+					+ e->Message,
+					"Полет",
+					MessageBoxButtons::OK,
+					MessageBoxIcon::Error);
+				this->Close();// закрываем форму return;
+			}
+			// сделать прозрачным фон
+			plane->MakeTransparent();
+			// установить размер формы равный
+			// размеру фонового рисунка
+			this->ClientSize = sky->Size;
+			// задать фоновый рисунок формы
+			this->BackgroundImage = gcnew Bitmap(sky);
+			// определяем графическую поверхность g
+			g = this->CreateGraphics();
+			// инициализация генератора случ. чисел
+			rnd = gcnew System::Random();
+			// исходное положение самолета
+			rct.X = -40;
+			rct.Y = 20 + rnd->Next(20);
+			rct.Width = plane->Width;
+			rct.Height = plane->Height;
+			/*
+			скорость полета определяется периодом
+			следования сигналов от таймера и величиной
+			приращения координаты X */
+			dx = 2; // скорость полета — 2 пиксела/тик_таймера
+			timer1->Interval = 20;
+			timer1->Enabled = true;
 		}
+	private: System::Windows::Forms::Timer^ timer1;
+	private: System::ComponentModel::IContainer^ components;
+	private:
+		Bitmap^ sky;
+
+		Bitmap^ plane;
+		// рабочая графическая поверхность
+		Graphics^ g;
+		// приращение координаты X, определяет скорость полета
+		int dx;
+		// область, в которой находится объект
+		Rectangle rct;
+		// генератор случайных чисел
+		System::Random^ rnd;
+		/*
+ private: System::Void InitializeComponent() {
+	 this->components = (gcnew System::ComponentModel::Container());
+	 this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+	 this->SuspendLayout();
+	 //
+	 // timer1
+	 //
+	 this->timer1->Tick += gcnew System::EventHandler(this, &TimerAnimationForm::timer1_Tick);
+	 //
+	 // TimerAnimationForm
+	 //
+	 this->ClientSize = System::Drawing::Size(284, 261);
+	 this->Name = L"TimerAnimationForm";
+	 this->ResumeLayout(false);
+
+ }*/
 
 	protected:
 		/// <summary>
@@ -34,12 +102,12 @@ namespace OOAtask85b {
 				delete components;
 			}
 		}
-
+	protected:
 	private:
 		/// <summary>
 		/// Обязательная переменная конструктора.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,12 +116,44 @@ namespace OOAtask85b {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"TimerAnimationForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->components = (gcnew System::ComponentModel::Container());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->SuspendLayout();
+			// 
+			// timer1
+			// 
+			this->timer1->Tick += gcnew System::EventHandler(this, &TimerAnimationForm::timer1_Tick);
+			// 
+			// TimerAnimationForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Name = L"TimerAnimationForm";
+			this->Text = L"TimerAnimationForm";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+	private: System::Void timer1_Tick(System::Object^ sender, System::EventArgs^ e) {
+		g->DrawImage(sky,
+			Rectangle(rct.X, rct.Y, rct.Width, rct.Height),
+			Rectangle(rct.X, rct.Y, rct.Width, rct.Height),
+			GraphicsUnit::Pixel);
+		// вычислить новое положение объекта
+		if (rct.X < this->ClientRectangle.Width)
+			rct.X += dx;
+		else
+		{
+			// объект достиг правой границы,
+			// перемещаем его к левой границе
+			rct.X = -40;
+			rct.Y = 20 + rnd->Next(40);
+			// скорость полета от 2 до 5
+			// пикселов/тик_таймера
+			dx = 2 + rnd->Next(4);
+		}
+		g->DrawImage(plane, rct.X, rct.Y);
+	}
 	};
 }
