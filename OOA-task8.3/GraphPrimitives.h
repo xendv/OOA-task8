23844f84a7b34ce47592e6b8ea3187a6b0765b7e
xendv <dvoynina.ksusha@mail.ru>
2021-04-13 22:03:21 +0300

    added task 8.3.в (pie diagram)

--- OOA-task8.3/GraphPrimitives.h
+++ OOA-task8.3/GraphPrimitives.h
@@ -59,7 +59,7 @@ namespace OOAtask83 {
 			this->pictureBox1->Dock = System::Windows::Forms::DockStyle::Fill;
 			this->pictureBox1->Location = System::Drawing::Point(0, 0);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(284, 261);
+			this->pictureBox1->Size = System::Drawing::Size(495, 484);
 			this->pictureBox1->TabIndex = 0;
 			this->pictureBox1->TabStop = false;
 			this->pictureBox1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &GraphPrimitives::pictureBox1_Paint);
@@ -68,7 +68,7 @@ namespace OOAtask83 {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->ClientSize = System::Drawing::Size(495, 484);
 			this->Controls->Add(this->pictureBox1);
 			this->Name = L"GraphPrimitives";
 			this->Text = L"GraphPrimitives";
@@ -103,12 +103,43 @@ namespace OOAtask83 {
 		g->DrawLines(Pens::DodgerBlue, p);
 
 		int x = x0, y = y0 + r * 3 / 2;
-
+		r = 7;
 		//рисуем точку (8.3.б)
-		g->FillEllipse(Brushes::ForestGreen, x, y, 7, 7);
+		g->FillEllipse(Brushes::ForestGreen, x-r/2, y, r, r);
 
 		//рисуем круговую диаграмму (8.3.в)
-
+		r =100;
+		x = x0  *2 / 3 - r/4 + 5;
+		//x -= r/2;
+		y +=40;
+		//рамка
+		Pen^ aPen = gcnew Pen(Color::Black,1);
+		g->DrawRectangle(aPen, x-10, y-20, 2*r+30, r+30);
+		aPen->Width = 2;
+		g->DrawEllipse(aPen, x, y, r, r);
+		//сектора
+		g->FillPie(Brushes::LightSteelBlue,
+			System::Drawing::Rectangle(x, y, r, r), 0, 90);
+		g->FillPie(Brushes::DarkBlue,
+			System::Drawing::Rectangle(x, y, r, r), 90, 110);
+		g->FillPie(Brushes::DarkViolet,
+			System::Drawing::Rectangle(x, y, r, r), 200, 20);
+		g->FillPie(Brushes::MediumPurple,
+			System::Drawing::Rectangle(x, y, r, r), 220, 150);
+		//подписи к диаграмме
+		g->DrawString("∆анры книг в наличии", this->Font, Brushes::Black, x + 50, y - 15);
+		x = x +  r + 20; y += 10;
+		g->FillRectangle(Brushes::LightSteelBlue, x, y, 10, 5);
+		g->DrawString("‘антастика", this->Font, Brushes::Black, x + 15, y-5);
+		y += 15;
+		g->FillRectangle(Brushes::DarkBlue, x, y, 10, 5);
+		g->DrawString(" лассика", this->Font, Brushes::Black, x + 15, y - 5);
+		y += 15;
+		g->FillRectangle(Brushes::DarkViolet, x, y, 10, 5);
+		g->DrawString("”чебна€", this->Font, Brushes::Black, x + 15, y - 5);
+		y += 15;
+		g->FillRectangle(Brushes::MediumPurple, x, y, 10, 5);
+		g->DrawString("Ёнциклопедии", this->Font, Brushes::Black, x + 15, y - 5);
 
 	}
 	};
